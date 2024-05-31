0453037e85ebcd648d9aee34fb07ed414417c3a3
wangshangxian <ws_xian@163.com>
2024-05-31 23:05:13 +0800

    todolist 合约

--- contracts/todo_list.sol
+++ contracts/todo_list.sol
@@ -12,11 +12,16 @@ contract todo_list {
 
     mapping(uint => task) public tasks;
 
+    event on_create_task(uint id);
+    event on_get_task(uint id, string content, bool completed);
+    event on_get_all_task(task[] tasks);
+
     constructor() {}
 
     function create_task(string memory _content) public returns (uint){
         task_count++;
         tasks[task_count] = task(task_count, _content, false);
+        emit on_create_task(task_count);
         return task_count;
     }
 
@@ -26,12 +31,17 @@ contract todo_list {
         tasks[_id] = _task;
     }
 
-    function get_task(uint _id) public view returns(uint, string memory, bool) {
+    function get_task(uint _id) public returns(uint, string memory, bool) {
         task memory _task = tasks[_id];
+        emit on_get_task(_task.id, _task.content, _task.completed);
         return (_task.id, _task.content, _task.completed);
     }
 
     function get_task_count() public view returns(uint) {
         return task_count;
+    }
+
+    function get_all_task() public returns (task[] tasks) {
+        emit on_get_all_task()
     }
 }
--- scripts/deploy_todolist.js
+++ scripts/deploy_todolist.js
@@ -5,13 +5,10 @@ async function main() {
 
     console.log('Deploying contracts with the account: ', deployer.address)
 
-    // const balance = await deployer.provider.getBalance()
-    // console.log('Account balance: ', ethers.utils.formatEther(balance))
-
     const todo_list_factory = await ethers.getContractFactory('todo_list')
     const contract = await todo_list_factory.deploy()
 
-    console.log('todo_list deployed to: ', contract.runner.address)
+    console.log('todo_list deployed to: ', contract.target)
 }
 
 main().then(() => process.exit(0)).catch(err => console.log(err) && process.exit(1))
